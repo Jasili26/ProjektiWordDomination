8da03bedb3d28951c58a3680c7476ed991ea343b
TuuniM <tuuni.mustonen@metropolia.fi>
2020-12-07 19:06:53 +0200

    tietoturva

--- core/pool.js
+++ core/pool.js
@@ -1,14 +1,17 @@
 const util = require('util');
 const mysql = require('mysql');
+require('dotenv').config();
 
 //Databaseen yhdistämistietoja
 
 const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
     connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: 'Pandabear69',
-    database: 'projektiDB'
+    queueLimit: 0,
 });
 
 pool.getConnection((err, connection) => {
