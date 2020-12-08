9f994ddb7c8cff4cc05b6f0b2848e73b3096cdf7
TuuniM <tuuni.mustonen@metropolia.fi>
2020-12-08 10:24:19 +0200

    nimivaihdettu

--- app2.js
+++ app2.js
@@ -2,26 +2,26 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const pageRouter = require('./routes/pages');
-const app = express();
+const app2 = express();
 
 
 // Kerätään käyttäjältä lähetettyjä tietoja
-app.use(express.urlencoded( { extended : false}));
+app2.use(express.urlencoded( { extended : false}));
 
 
 // Jaetaan tiedostot publc kansiosta käyttöön
-app.use(express.static(path.join(__dirname, 'public')));
+app2.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app2.use('/static', express.static(path.join(__dirname, 'public')))
 
 
 
 // Template engine. PUG
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app2.set('views', path.join(__dirname, 'views'));
+app2.set('view engine', 'pug');
 
 // sessiot
-app.use(session({
+app2.use(session({
     secret:'kouluonkivaa',
     resave: false,
     saveUninitialized: false,
@@ -32,26 +32,26 @@ app.use(session({
 
 
 // Routersit
-app.use('/', pageRouter);
+app2.use('/', pageRouter);
 
 
 // Virheilmoituksen asettaminen
-app.use((req, res, next) =>  {
+app2.use((req, res, next) =>  {
     var err = new Error('Page not found');
     err.status = 404;
     next(err);
 })
 
 // Virheiden käsittely. Lähetetään käyttäjälle.
-app.use((err, req, res, next) => {
+app2.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.send(err.message);
 });
 
 // Asetetaan kuuntelemaan porttia
-app.listen(3001, () => {
-    console.log('Server is running on port 3000...');
+app2.listen(3001, () => {
+    console.log('Server is running on port 3001...');
 });
 
 
-module.exports = app;
+module.exports = app2;
